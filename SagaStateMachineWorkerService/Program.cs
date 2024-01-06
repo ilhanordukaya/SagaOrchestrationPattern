ce0ff20a59a27cbba08cbb281b2e54dcf26f4e1c
ilhanordukaya <ilhanordukaya69@gmail.com>
2024-01-06 21:29:37 +0300

    stock service config masstransit

--- SagaStateMachineWorkerService/Program.cs
+++ SagaStateMachineWorkerService/Program.cs
@@ -1,8 +1,42 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using SagaStateMachineWorkerService;
+using SagaStateMachineWorkerService.DataAccess;
+using SagaStateMachineWorkerService.Models;
+using Shared;
+using System.Reflection;
 
 IHost host = Host.CreateDefaultBuilder(args)
-	.ConfigureServices(services =>
+	.ConfigureServices((hostContext,services) =>
 	{
+		services.AddMassTransit( cfg=>
+		{
+			cfg.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>().EntityFrameworkRepository(opt =>
+			{
+				opt.AddDbContext<DbContext, OrderStateDbContext>((provider,builder)=>
+				{
+					builder.UseNpgsql(hostContext.Configuration.GetConnectionString("Postgres"), m =>
+					{
+						m.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+					});
+				});
+			});
+
+			cfg.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(configure =>
+			{
+				configure.Host(hostContext.Configuration.GetConnectionString("RabbitMQ"));
+
+				configure.ReceiveEndpoint(RabbitMQSettingsConst.OrderSaga, e =>
+				{
+					e.ConfigureSaga<OrderStateInstance>(provider);
+				});
+			}));
+		
+	});
+
+
+		
+		//services.AddMassTransitHostedService();
 		services.AddHostedService<Worker>();
 	})
 	.Build();
--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,11 +1,41 @@
+using MassTransit;
+using Shared;
+using Stock.API.Consumers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddMassTransit(x =>
+{
+	x.AddConsumer<OrderCreatedEventConsumer>();
+
+	x.AddConsumer<StockRollBackMessageConsumer>();
+
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+
+		cfg.ReceiveEndpoint(RabbitMQSettingsConst.StockOrderCreatedEventQueueName, e =>
+		{
+			e.ConfigureConsumer<OrderCreatedEventConsumer>(context);
+		});
+
+		cfg.ReceiveEndpoint(RabbitMQSettingsConst.StockRollBackMessageQueueName, e =>
+		{
+			e.ConfigureConsumer<StockRollBackMessageConsumer>(context);
+		});
+	});
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
